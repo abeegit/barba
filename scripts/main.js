0cd64f291a0d8b954b104e507fad908fb2821525
Abhijeet Kumar <abhijeet.kumar@22feettribalww.com>
2018-05-24 19:57:46 +0530

    WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
 (function() {
-    // $(".sliding-menu").slick({
-    //     infinite: false,
-    //     focusOnChange: true,
-    //     speed: 600,
-    //     arrows: false,        
-    // })
+
+    $('.nav-carousel').slick({
+        infinite: false,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+    function animateLogo() {
+        TweenMax.to(document.querySelector(".logo"), 4, { "background-color": "#8B6969" });
+    };
     
     function initialize() {
+        animateLogo();
         var items = document.querySelectorAll(".menu-item");
         for ( var i = 0; i < items.length; i++ ) {
             var item = items[i];
